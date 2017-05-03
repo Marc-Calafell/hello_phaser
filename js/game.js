97ffc4fa471b7121128b19bbcd50187e324ff01a
Marc-Calafell <marccalafell95@gmail.com>
2017-05-03 19:24:32 +0200

    Initial version

---
+++ js/game.js
@@ -0,0 +1,19 @@
+/**
+ * Created by mark on 3/05/17.
+ */
+console.log("qasdasdasd")
+
+window.load = function () {
+
+    var states = {
+        preload: preload,
+        create: create
+    }
+    var game = new Phaser(800,600,Phaser.AUTO,'', states)
+
+
+    function preload() {
+        game.load().image
+    }
+
+}
